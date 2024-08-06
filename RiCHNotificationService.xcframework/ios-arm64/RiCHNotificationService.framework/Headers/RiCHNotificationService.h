9e11df317b95247997ca9c84789fee3e43240d14
Diaa Salam <d.salam@t2.sa>
2024-08-06 17:32:44 +0300

    logging request notification content

---
+++ RiCHNotificationService.xcframework/ios-arm64/RiCHNotificationService.framework/Headers/RiCHNotificationService.h
@@ -0,0 +1,18 @@
+//
+//  RiCHNotificationService.h
+//  RiCHNotificationService
+//
+//  Created by Omar on 10/28/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RiCHNotificationService.
+FOUNDATION_EXPORT double RiCHNotificationServiceVersionNumber;
+
+//! Project version string for RiCHNotificationService.
+FOUNDATION_EXPORT const unsigned char RiCHNotificationServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RiCHNotificationService/PublicHeader.h>
+
+
