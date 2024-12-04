84d15c633bea6772caac838e555e7cf637b478a9
Diaa Salam <d.salam@t2.sa>
2024-12-04 13:13:53 +0200

    update swift package & xcframework for the latest Xcode version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+
 let package = Package(
     name: "RiCHNotificationService",
-    platforms: [
-        .iOS(.v11)
-    ],
     products: [
         .library(
             name: "RiCHNotificationService",
